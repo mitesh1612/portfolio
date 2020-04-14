0b09f42672a3cc80493740a598fd70b8799736ad
Mitesh Shah <mitsha@microsoft.com>
2020-04-14 15:56:07 +0530

    Updates

---
+++ precache-manifest.020e5732d9a8437d6a522d427cc1afd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e02156e2592784f7af715667220763",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aadb5695f784df2fde13",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "6aee880df692f13efbec",
+    "url": "/portfolio/static/js/2.512e11ac.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/portfolio/static/js/2.512e11ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aadb5695f784df2fde13",
+    "url": "/portfolio/static/js/main.0a09fc53.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
